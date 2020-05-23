cfc634ebbec03618c8e456015a0fca292b80f4f0
Geena Kim <geena@Geenas-Air.fios-router.home>
2020-05-22 20:39:26 -0400

    skeletons are donezo

--- js/main.js
+++ js/main.js
@@ -4,4 +4,9 @@ $(document).ready(function () {
 
   $('#page01 .container-box').css('margin-top', topalign);
   // var bgColors  = ["#FBEAD3", "#FBD3E4", "#D3F8FB", "#F7C9CF", "#D3FBEA", "#AEEEEE"]
+
+  var back = ["#FBEAD3", "#FBD3E4", "#D3F8FB", "#F7C9CF", "#D3FBEA", "#AEEEEE"];
+  var rand = back[Math.floor(Math.random() * back.length)];
+  $('body').css('background',rand);
+
 });
